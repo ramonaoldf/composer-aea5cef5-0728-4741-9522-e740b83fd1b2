a9bd5a49564b554ff7d7292864e494f3b1471582
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 18:36:52 -0300

    Vesion: 001.005.000.000

--- types/@types/laravel-nova-ui/Button.vue.d.ts
+++ types/@types/laravel-nova-ui/Button.vue.d.ts
@@ -58,11 +58,11 @@ declare const _default: import("vue").DefineComponent<import("vue").ExtractPropT
     trailingIcon: {};
 }>> & Readonly<{}>, {
     size: string;
-    as: string;
     variant: string;
+    disabled: boolean;
+    as: string;
     state: string;
     padding: string;
     loading: boolean;
-    disabled: boolean;
 }, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
 export default _default;
